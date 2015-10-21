e7d7e9898414dbc5ed94fd9fb60cbfc23e765693
hddwqm <hddwqm@gmail.com>
2015-10-21 15:34:15 -0500

    syntax correction

--- InMemoryFileSystem/FileSystem.h
+++ InMemoryFileSystem/FileSystem.h
@@ -1,4 +1,7 @@
 #pragma once
+
+using namespace std;
+
 class FileSystem {
 	folder root;
 public:
@@ -11,8 +14,11 @@ public:
 		folder* fs_root = &root;
 		fs_base* it = &root;
 		for (int index = 0;index < folder_path.size;index++) {
-			if (it->get_member(folder_path[index]) != NULL) {
-				it = it->get_member(folder_path[index];
+			if (it->get_member(folder_path[index]) != nullptr) {
+				it = it->get_member(folder_path[index]);
+			}
+			else {
+				// IMPLEMENT ME
 			}
 		}
 		vector<string> output;
