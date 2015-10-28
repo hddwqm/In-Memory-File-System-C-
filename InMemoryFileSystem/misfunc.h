54bf1a68c995a3a6517379cf83e3776df2dfd77d
hddwqm <hddwqm@gmail.com>
2015-10-27 22:45:56 -0500

    Changed structure

---
+++ InMemoryFileSystem/misfunc.h
@@ -0,0 +1,25 @@
+#include"prjheader.h"
+
+std::vector<std::string> split(const std::string &s, char delim) {
+	std::vector<std::string> elems;
+	std::stringstream ss(s);
+	std::string item;
+	while (getline(ss, item, delim)) {
+		elems.push_back(item);
+	}
+	return elems;
+}
+
+std::string join(const std::vector<std::string>& input) {
+	std::string output = "";
+	bool has_data = false;
+	for (size_t i = 0; i < input.size(); ++i) {
+		has_data = true;
+		output += input[i];
+		output += ", ";
+	}
+	if (has_data) {
+		output = output.substr(0, output.length() - 2);
+	}
+	return output;
+}
